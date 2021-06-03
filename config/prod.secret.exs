0fb411c17e6eb35c677050d2ae159e7d13edfca3
Peter Ullrich <PJUllrich@users.noreply.github.com>
2021-06-03 14:34:36 +0200

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :app, App.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "3")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
