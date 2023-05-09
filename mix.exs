9cd0a340437af10708f9b00cec2a3172e28d8ae0
Peter Ullrich <{ID}+{username}@users.noreply.github.com>
2023-05-09 09:49:25 +0200

    I don't know what I'm doing

--- mix.exs
+++ mix.exs
@@ -65,11 +65,7 @@ defmodule App.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
-      "assets.deploy": [
-        "cmd --cd assets npm run deploy",
-        "esbuild default --minify",
-        "phx.digest"
-      ]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
 end
